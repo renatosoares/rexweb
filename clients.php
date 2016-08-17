85d352068c774162d2bcb7c55296632fa4303a06
Renato Soares <renatosoarespro@hotmail.com>
2016-08-17 13:22:12 -0300

    área dos clientes

---
+++ clients.php
@@ -0,0 +1,79 @@
+<?php  require_once 'head.php'; ?>
+<body>
+
+<!-- Header -->
+<?php require_once 'header.php'; ?>
+
+<div class="clear"></div>
+<div style="margin-top:-90px">
+
+<!-- Short Banner -->
+<div class="tall-banner" style="background:url(img/Banner/banner_11.jpg) no-repeat center center; background-size:cover">
+    <div class="container overlay overlay2">
+      <!--<h1 align="center" style="padding-top:300px;">Clients</h1>-->
+      <!--<h2 align="center">sit amet Consectetur adipiscing elit</h2>-->
+      <!--<div class="text-center p-t-20"><a href="#" class="btn  btn-primary">READ MORE</a></div>-->
+    </div>
+
+  </div>
+
+ <div class="clear"></div>
+
+ <!-- Section 1-->
+<section id="section1" class=" bg-grayL relative">
+    <div class="container p-t-40">
+
+      <div class="col-sm-12 well-lg  wow fadeInUp">
+        <h4 class="M0 p-t-30 font38">Case Studies</h4>
+        <div style="border-top:1px solid #ccc; margin-top:20px; margin-bottom:20px; float:none; width:100px;"></div>
+
+        <p class="text-gray-dark font14 m-t-35 m-b-15">Our passion for the work we do for our clients are reflected in what we deliver for them. Below are case studies of a small sample of work we have done over the years in online marketing, web development, and web design. <br/>
+        <br/>Our design team focused primarily on User Interface and worked directly with the client to create a social networking platform serving the environmentally conscious community.
+        </p>
+        <a href="client-1.html" class="btn btn-primary m-t-10">Learn More</a> </div>
+
+      <!--<div class="col-sm-6  wow fadeInRight  img4"> <img src="img/client/img1.png" class="img-responsive"/> </div>-->
+
+    </div>
+  </section>
+
+<div class="shapeTri"></div>
+
+<div class="clear p-t-20"></div>
+
+
+
+<!-- SECTION 2 -->
+<section>
+	<div class="container">
+    	<div class="col-sm-12  wow fadeInUp ">
+
+        <div class="col-sm-4 P20 text-center  P40">
+         <div class="lineD">
+        	<img src="img/client/aventureiro_pipa.png"class="img-rounded">
+            <div style="border-top:1px solid #ccc; margin-top:20px; margin-bottom:20px; float:none; width:200px; margin:0 auto;"></div>
+            <p class="boldL m-t-20 text-uppercase font16">Aventureiro Pipa</p>
+            <a href="http://www.aventureiropipa.com.br/" class="btn btn-primary">Visite o site</a>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis feugiat sem eu nunc porttitor interdum.</p>
+            <div class="p-t-40"></div>
+          </div>
+        </div>
+
+        <div class="col-sm-4 P20 text-center  P40">
+         <div class="lineD">
+          <img src="img/client/reino_codigo.png"class="img-rounded">
+            <div style="border-top:1px solid #ccc; margin-top:20px; margin-bottom:20px; float:none; width:200px; margin:0 auto;"></div>
+            <p class="boldL m-t-20 text-uppercase font16">Reino do Código</p>
+            <a href="https://fierce-badlands-86576.herokuapp.com" class="btn btn-primary">Visite o site</a>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis feugiat sem eu nunc porttitor interdum.</p>
+            <div class="p-t-40"></div>
+          </div>
+        </div>
+
+    </div>
+    </div>
+</section>
+
+<div class="clear P30"></div>
+
+<?php require_once 'footer.php'; ?>
--- header.php
+++ header.php
@@ -4,19 +4,19 @@
 		<div class="col-xs-3 col-sm-9 P0 text-right">
 			<nav class="navbar" id="nav">
 				<ul id="menu">
-					
+
 					<li><a href="index.php">Rex</a></li>
 					<li class="drop"><a href="dev.php">Serviços</a></li>
 					<li class="drop"><a href="about.html">Sobre</a></li>
-					<li><a href="clients.html">Clientes</a></li>
+					<li><a href="clients.php">Clientes</a></li>
 					<li><a href="contact.html">Contato</a></li>
-					
+
 				</ul>
-				<!-- Navigation End --> 
+				<!-- Navigation End -->
 			</nav>
 		</div>
 	</div>
 </header>
 <div class="clear"></div>
 
-<div style="margin-top:-90px;"> 
+<div style="margin-top:-90px;">
