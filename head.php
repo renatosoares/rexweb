5e247beefc0d82f38b3fee7bf3b0d2895a13e7a3
Renato Soares <renatosoarespro@hotmail.com>
2016-11-13 17:35:11 -0300

    #2 verificar site debugar

--- head.php
+++ head.php
@@ -1,33 +1,32 @@
 <!doctype html>
 <html>
 
-<!-- Mirrored from usaweb.net/ by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 22 Sep 2015 18:01:15 GMT -->
 <head>
-<meta charset="utf-8">
-<title>RexWeb&reg; - Web Solutions | Innovative Web &amp; Mobile App Development Company</title>
-<meta name='description' content='Web Solutions Company, Trusted since 1999 by leading brands and agencies as their go-to web department for, custom web development, mobile application development, cloud marketing, UI/UX design, online marketing services and Strategic Consultation.' />
-<meta name='keywords' content='web solutions, online marketing services, mobile app development Company, web department, custom web development' />
-<meta name="author" content="">
+    <meta charset="utf-8">
+    <title>RexWeb&reg; - Web Solutions | Innovative Web &amp; Mobile App Development Company</title>
+    <meta name='description' content='Web Solutions Company, Trusted since 1999 by leading brands and agencies as their go-to web department for, custom web development, mobile application development, cloud marketing, UI/UX design, online marketing services and Strategic Consultation.' />
+    <meta name='keywords' content='web solutions, online marketing services, mobile app development Company, web department, custom web development' />
+    <meta name="author" content="">
 
-<link rel="shortcut icon" href="img/favicon.ico">
-<!-- Bootstrap Core CSS -->
-<link href="css/bootstrap.css" rel="stylesheet">
-<!-- Custom CSS -->
-<link href="css/main-styles.css" rel="stylesheet">
-<!-- Dropdown menu -->
-<link rel="stylesheet" href="css/nav-styles.css">
-<!-- Custom Fonts -->
-<link href="css/animate.css" rel="stylesheet" type="text/css">
-<link href="css/custom.css" rel="stylesheet" type="text/css">
-<!-- Custom Fonts -->
-<link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-<!-- Owl Carousel Assets -->
-<link href="css/owl.carousel.css" rel="stylesheet">
-<!-- popup -->
-<link rel="stylesheet" type="text/css" href="css/popup.css">
+    <link rel="shortcut icon" href="img/favicon.ico">
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/main-styles.css" rel="stylesheet">
+    <!-- Dropdown menu -->
+    <link rel="stylesheet" href="css/nav-styles.css">
+    <!-- Custom Fonts -->
+    <link href="css/animate.css" rel="stylesheet" type="text/css">
+    <link href="css/custom.css" rel="stylesheet" type="text/css">
+    <!-- Custom Fonts -->
+    <link href="font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <!-- Owl Carousel Assets -->
+    <link href="css/owl.carousel.css" rel="stylesheet">
+    <!-- popup -->
+    <link rel="stylesheet" type="text/css" href="css/popup.css">
 
-<link href="css/style.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
 
-<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
 </head>
 <body>
--- index.php
+++ index.php
@@ -1,11 +1,11 @@
+<!-- Header -->
 <?php  require_once 'head.php'; ?>
 
 
-<!-- Header -->
 <?php require_once 'nav.php'; ?>
 
 <!-- Banner -->
-<div id="slider1_container" style="height:760px; width:1920px; max-height:100%;">
+<div id="slider1_container">
 	<!-- Loading Screen -->
 	<div u="loading" style="position: absolute; top: 0px; left: 0px;">
 		<div style="position: absolute; display: block; background:#fcfcfc; top: 0px; left: 0px; width: 100%; height: 760px; text-align:center;"> <img src="img/loading.gif" style=" margin-top:18%;"> </div>
@@ -56,169 +56,7 @@
 
 				<!-- Arrow -->
 				<span u="arrowleft" class="jssora21l"  style="width: 35px; height: 45px; left:18px;"></span> <span u="arrowright" class="jssora21r" style="width: 35px; height: 45px; right:18px;"></span>
-			</div>
-
-<!-- Section 1  |  Web Development-->
-<section id="section1" class=" bg-grayL">
-	<div class="container relative">
-
-		<!--Left Content -->
-		<div class=" col-md-6 col-sm-12  well-lg  wow fadeInLeft animated">
-			<h3 class="M0 p-t-50 relative">Web responsivo</h3>
-			<div class="clear p-t-20" style="border-bottom:1px solid #ccc; width:100px;"></div>
-			<p class="text-gray-dark font14 m-t-35 m-b-15">
-
-				REXWEB tem sido um parceiro inovador para agências de
-				marketing e soluções provedor desde 1999. Através de nossos
-				parceiros podemos atender a uma ampla raiva de clientes em
-				todo o mundo com serviços de desenvolvimento de aplicativos
-				móveis e web personalizado, bem como produtos de marketing
-				online.
-
-			</p>
-
-			<a href="dev.html" class="btn btn-primary m-t-10">+ Serviços</a> </div>
-
-			<!-- Right Img -->
-			<div class=" col-md-6 col-sm-12 p-t-20  wow fadeInRight animated">
-				<div class="img1">
-					<img src="img/home/img1.png" class="img-responsive"/>
-				</div>
-			</div>
-
-		</div>
-</section>
-
-<!-- Shape 1 -->
-<div class="shapeTri"></div>
-<div class="clear P40"></div>
-
-<!-- Section 2 |  Apps Development -->
-<section id="section1" class="">
-	<div class="container">
-
-		<div class="col-sm-6 p-t-20  wow fadeInLeft animated"> <img src="img/home/img2.png" class="img-responsive"/> </div>
-
-		<div class="col-sm-6 well-lg  wow  fadeInRight animated">
-			<h3 class="M0 p-t-30">Desenvolvimento app mobile</h3>
-			<div class="clear p-t-20" style="border-bottom:1px solid #ccc; width:100px;"></div>
-			<p class="text-gray-dark font14 m-t-35 m-b-15">
-
-				Nós consultar com você desde a concepção até a implantação para
-				garantir que o fim produto está em linha com suas expectativas. Nós
-				nos concentramos nas necessidades de seus usuários para garantir que
-				sua experiência será excepcional e ainda ficar em linha com os seus
-				objectivos de negócio.
-
-			</p>
-			<a href="dev.html" class="btn btn-primary m-t-10">+ Serviços</a>
-		</div>
-
-	</div>
-</section>
-
-  <!-- Section: about -->
-  <section id="about" class="home-section text-center">
-	<div class="heading-about">
-	  <div class="container">
-		  <div class="row">
-			<div class="col-lg-8 col-lg-offset-2">
-			  <div class="wow bounceInDown" data-wow-delay="0.4s">
-				  <div class="section-heading">
-					  <h2>Nossa equipe fantástica</h2>
-					  <i class="fa fa-2x fa-angle-down"></i>
-
-				  </div>
-			  </div>
-		  </div>
-	  </div>
-  </div>
-</div>
-<div class="container">
-
-	<div class="row">
-	  <div class="col-lg-2 col-lg-offset-5">
-		<hr class="marginbot-50">
-	</div>
-</div>
-<div class="row">
-	<div class="col-xs-6 col-sm-4 col-md-4">
-		<div class="wow bounceInUp" data-wow-delay="0.2s">
-			<div class="team boxed-grey">
-				<div class="inner">
-					<h5>Italo Berg</h5>
-					<p class="subtitle">Back end</p>
-					<div class="avatar"><img src="img/team/italo-berg-pb.jpg" alt="" class="img-responsive img-circle" /></div>
-					<ul class="list-inline social-buttons">
-						<li><a href="http://plus.google.com/+ItaloBergReis"><i class="fa fa-google-plus"></i></a></li>
-						<li><a href="http://github.com/iceberg20"><i class="fa fa-github-alt"></i></a></li>
-
-						<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="col-xs-6 col-sm-4 col-md-4">
-		<div class="wow bounceInUp" data-wow-delay="0.5s">
-			<div class="team boxed-grey">
-				<div class="inner">
-					<h5>Julian Sansat</h5>
-					<p class="subtitle">Back end</p>
-					<div class="avatar"><img src="img/team/julian-sansat-pb.jpg" alt="" class="img-responsive img-circle" /></div>
-					<ul class="list-inline social-buttons">
-
-						<li><a href="#"><i class="fa fa-facebook"></i></a></li>
-							<li><a href="http://github.com/JulianSansat"><i class="fa fa-github-alt"></i></a></li>
-						<li><a href="http://www.linkedin.com/in/juliansansat"><i class="fa fa-linkedin"></i></a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="col-xs-6 col-sm-4 col-md-4">
-	  <div class="wow bounceInUp" data-wow-delay="0.8s">
-		<div class="team boxed-grey">
-		  <div class="inner">
-			<h5>Renato Soares</h5>
-			<p class="subtitle">Front end</p>
-			<div class="avatar"><img src="img/team/renato-soares.jpg" alt="" class="img-responsive img-circle" /></div>
-			<ul class="list-inline social-buttons">
-				<li><a href="https://twitter.com/renatosoarespro"><i class="fa fa-twitter"></i></a></li>
-				<li><a href="http://github.com/renatosoares"><i class="fa fa-github-alt"></i></a></li>
-				<li><a href="https://br.linkedin.com/in/renatosoarespro"><i class="fa fa-linkedin"></i></a></li>
-			</ul>
-		</div>
-	</div>
-</div>
 </div>
 
-</div>
-</div>
-</section>
-  <!-- /Section: about -->
-
-
-<div class="clear P10 lineB container"></div>
-<div class="" style=" padding-bottom:100px;"></div>
-<!-- Shape 1 -->
-<div class="shapeTri2"></div>
-<!-- Form -->
-<section class="bg-grayL relative">
-	<div class="container text-center P30  wow fadeInDown ">
-
-		<div class="col-sm-2"></div>
-		<div class="col-sm-8 contact P30">
-			<h4 class="text-center">Solicitar Mais Informações</h4>
-			<div class="clear " style="border-top:1px solid #ccc; padding-bottom:40px;  width:100px; margin:0 auto;"></div>
-
-			<?php require_once 'form-contact.php'; ?>
-
-			<div class="clear"></div>
-		</div>
-
-		<div class="clear"></div>
-	</div>
-</section>
 <!-- Footer -->
 <?php require_once 'footer.php'; ?>
